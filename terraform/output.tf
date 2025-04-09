c3f3404c420d92216b4ccbee16a73f0ff0291615
bishoy <bishoyrefaat95@gmail.com>
2025-04-09 23:08:59 +0200

    terraform apply

--- terraform/output.tf
+++ terraform/output.tf
@@ -8,12 +8,12 @@ output "ec2-public-ip-us-east-1b" {
 }
 
 
-output "ec2-private-ip-us-east-1b" {
-  description = "get the public ip"
-  value       = module.ec2-private-us-east-1b.ec2-private-ip
-}
-output "ec2-private-ip-us-east-1a" {
-  description = "get the public ip"
-  value       = module.ec2-private-us-east-1a.ec2-private-ip
+# output "ec2-private-ip-us-east-1b" {
+#   description = "get the public ip"
+#   value       = module.ec2-private-us-east-1b.ec2-private-ip
+# }
+# output "ec2-private-ip-us-east-1a" {
+#   description = "get the public ip"
+#   value       = module.ec2-private-us-east-1a.ec2-private-ip
 
-}
+# }
